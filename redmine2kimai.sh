eaaf7ae31e8c38483fae31216f635989fbb23278
Oliver <javadoc@email.de>
2017-03-25 19:06:22 +0100

    login paths not supported by dockerized mysql

--- redmine2kimai.sh
+++ redmine2kimai.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LANG=de_DE.UTF-8
-docker exec mysql mysql --login-path=kimai redmine2kimai -e 'CALL fill_time_fields();'
-docker exec mysql mysql --login-path=root  redmine2kimai -e 'CALL import_in_kimai();'
+docker exec mysql mysql -u kimai --password=100%Jondos redmine2kimai -e 'CALL fill_time_fields();'
+docker exec mysql mysql -u root  --password=100%Jondos redmine2kimai -e 'CALL import_in_kimai();'
--- run-mysql.sh
+++ run-mysql.sh
@@ -10,9 +10,3 @@ docker run \
   --name mysql \
   -v $HOME/mysql:/var/lib/mysql \
   mysql/mysql-server
-
-echo Enter password for MYSQL user root:
-docker exec mysql mysql_config_editor set --login-path=root  --host=localhost --user=kimai --password
-
-echo Enter password for MYSQL user kimai:
-docker exec mysql mysql_config_editor set --login-path=kimai --host=localhost --user=kimai --password
