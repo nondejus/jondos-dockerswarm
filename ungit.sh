c454cb461a44b8360b6aa1bafc970371221808ca
Oliver Meyer <zudljk@email.de>
2017-08-14 08:14:36 +0000

    Icinga and satellite systems

---
+++ ungit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# https://community.c9.io/t/ungit-git-gui-in-browser/2804
+
+echo "http://jondos-dockerswarm-zudljk.c9users.io:8081/#/repository?path=%2Fhome%2Fubuntu%2Fworkspace"
+
+ungit --urlBase http://0.0.0.019 --port 8081
+
