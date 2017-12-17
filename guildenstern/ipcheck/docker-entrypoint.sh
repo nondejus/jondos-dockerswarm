90e0ad3d1b4e27f521b123118d56463f5db81a17
zudljk <zudljk@email.de>
2017-12-17 16:52:51 +0000

    Added some config for ipcheck

--- guildenstern/ipcheck/docker-entrypoint.sh
+++ guildenstern/ipcheck/docker-entrypoint.sh
@@ -4,7 +4,9 @@ service mysql start
 
 java -server -Djava.awt.headless=true -jar /usr/share/java/InfoService.jar /etc/infoservice/InfoService.properties &
 
-java -cp /var/www/anontest/ftptest testftpserver.FTPServer > /var/log/ftptest.log &
+/etc/cron.weekly/updategeoip
+
+cd /var/www/anontest/ftptest && java -cp . testftpserver.FTPServer >/var/log/ftptest.log 2>&1 &
 
 /usr/bin/tor --defaults-torrc /usr/share/tor/tor-service-defaults-torrc --hush & 
 
