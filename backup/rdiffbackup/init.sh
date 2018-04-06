35a5e14723f7d85c568a299b35a94d25cfc260c3
zudljk <zudljk@email.de>
2018-04-06 10:12:53 +0200

    Add scripts for backup

--- backup/rdiffbackup/init.sh
+++ backup/rdiffbackup/init.sh
@@ -6,6 +6,5 @@
 ssh-keygen -A
 
 # generate backup user's ssh key
-ssh-keygen -f /home/backup/.ssh/id_rsa -t rsa -N ''
-
-echo command=\"rdiff-backup --server --restrict-read-only /\",no-port-forwarding,no-X11-forwarding,no-pty $(</home/backup/.ssh/id_rsa.pub) >> /home/backup/.ssh/authorized_keys
+mkdir -p /etc/rdiffweb/clients
+ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
