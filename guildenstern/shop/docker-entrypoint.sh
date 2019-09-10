a4bfc6b5a34c3fe24c3dd263758403b1a61105d7
zudljk <zudljk@email.de>
2019-09-10 23:36:49 +0100

    Update shop

--- guildenstern/shop/docker-entrypoint.sh
+++ guildenstern/shop/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+chgrp -R mysql /var/lib/mysql
+chmod -R g+rwX /var/lib/mysql
+
 service mysql start
 
-service tomcat8 start
+/opt/tomcat9/bin/startup.sh &
 
 service postfix start
 
